95cd1fb49774db4de50c67964c358213a7299616
Programming Hero <programming.hero1@gmail.com>
2024-11-19 20:56:02 -0600

    structure add coffee form

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -2,37 +2,74 @@ import React from 'react';
 
 const AddCoffee = () => {
     return (
-        <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
-                </div>
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <form className="card-body">
-                        <div className="form-control">
+        <div className='lg:w-3/4 mx-auto'>
+            <div className="text-center p-10">
+                <h1 className="text-5xl font-bold">Add Coffee!</h1>
+                <p className="py-6">
+                    Provident cupiditate voluptatem et in.Quaerat fugiat ut assumenda excepturi exercitationem
+                    quasi. In deleniti eaque aut repudiandae et a id nisi.
+                </p>
+            </div>
+            <div className="card bg-base-100 w-full shrink-0 shadow-2xl">
+                <form className="card-body">
+                    {/* form first row */}
+                    <div className='flex flex-col lg:flex-row gap-5'>
+                        <div className="form-control flex-1">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="label-text">Name</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input type="text" name='name' placeholder="coffee name" className="input input-bordered" required />
                         </div>
-                        <div className="form-control">
+                        <div className="form-control flex-1">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="label-text">Chef</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input type="text" name='chef' placeholder="chef name" className="input input-bordered" required />
+                        </div>
+                    </div>
+                    {/* form second row */}
+                    <div className='flex flex-col lg:flex-row gap-5'>
+                        <div className="form-control flex-1">
                             <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                <span className="label-text">Supplier</span>
                             </label>
+                            <input type="text" name='supplier' placeholder="coffee supplier" className="input input-bordered" required />
                         </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                        <div className="form-control flex-1">
+                            <label className="label">
+                                <span className="label-text">Taste</span>
+                            </label>
+                            <input type="text" name='taste' placeholder="taste name" className="input input-bordered" required />
                         </div>
-                    </form>
-                </div>
+                    </div>
+                    {/* form third row */}
+                    <div className='flex flex-col lg:flex-row gap-5'>
+                        <div className="form-control flex-1">
+                            <label className="label">
+                                <span className="label-text">Category</span>
+                            </label>
+                            <input type="text" name='category' placeholder="coffee Category" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control flex-1">
+                            <label className="label">
+                                <span className="label-text">Details</span>
+                            </label>
+                            <input type="text" name='details' placeholder="Coffee Details" className="input input-bordered" required />
+                        </div>
+                    </div>
+
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <input type="text" name='photo' placeholder="Photo url" className="input input-bordered" required />
+                        
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Add Coffee</button>
+                    </div>
+                </form>
             </div>
         </div>
     );
